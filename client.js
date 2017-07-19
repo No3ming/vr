bbf1ab1f8b29381ceb65140f6b64f81b45477856
lidm <lidm@cloudream.com>
2017-07-19 11:12:03 +0800

    update

--- client.js
+++ client.js
@@ -6,7 +6,7 @@
 import {VRInstance} from 'react-vr-web';
 
 function init(bundle, parent, options) {
-  const vr = new VRInstance(bundle, 'oneVR', parent, {
+  const vr = new VRInstance(bundle, 'reactVR', parent, {
     // Add custom options here
     ...options,
   });
